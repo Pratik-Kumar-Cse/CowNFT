4e67f178df66705b858e2c43ca156dcee4da8d9c
Pratik Kumar <pk@rapidinnovation.dev>
2022-02-10 17:22:59 +0530

    new

---
+++ contracts/CowNFT.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2 ;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Authorizable is Ownable {
+
+    mapping(address => bool) public authorized;
+    address[] public adminList;
+
+    event AddAuthorized(address indexed _address);
+    event RemoveAuthorized(address indexed _address, uint index);
+
+    modifier onlyAuthorized() {
+        require(authorized[msg.sender] || owner() == msg.sender,"Authorizable: caller is not the SuperAdmin or Admin");
+        _;
+    }
+
+    function addAuthorized(address _toAdd) onlyOwner public {
+        require(_toAdd != address(0),"Authorizable: _toAdd isn't vaild address");
+        authorized[_toAdd] = true;
+        adminList.push(_toAdd);
+        emit AddAuthorized(_toAdd);
+    }
+
+    function removeAuthorized(address _toRemove,uint _index) onlyOwner public {
+        require(_toRemove != address(0),"Authorizable: _toRemove isn't vaild address");
+        require(adminList[_index] == _toRemove,"Authorizable: _index isn't valid index");
+        authorized[_toRemove] = false;
+        delete adminList[_index];
+        emit RemoveAuthorized(_toRemove,_index);
+    }
+
+    function getAdminList() public view returns(address[] memory ){
+        return adminList;
+    }
+
+}
+
+contract NFT4Play is ERC721, Authorizable {
+
+    using Strings for uint256;
+    using SafeMath for uint256;
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    uint256 public counter = 0;
+
+    uint256 public totalSupply;
+
+    string public faction;
+
+    string private baseURI_;
+
+    // Mapping from holder address to their (enumerable) set of owned tokens
+    mapping(address => EnumerableSet.UintSet) private _holderTokens;
+    mapping(uint256 => string) public tokenFaction;
+
+    constructor(string memory _baseTokenURI,string memory _factionName) public ERC721("NFT4Play", "4Play") {
+        _setBaseURI(_baseTokenURI);
+        faction = _factionName;
+        totalSupply = 10000;
+    }
+
+    function tokenOfOwnerByIndex(address _owner, uint256 _index) public view returns (uint256){
+        return _holderTokens[_owner].at(_index);
+    }
+
+    function _setBaseURI(string memory _baseTokenURI) internal {
+        baseURI_ = _baseTokenURI;
+    }
+
+    function changeFaction(string memory _factionName) public onlyOwner(){
+        faction = _factionName;
+    }
+
+    function changeTotalSupply(uint256 _totalSupply) public onlyOwner(){
+        totalSupply = _totalSupply;
+    }
+
+    function mint(address _to) public onlyAuthorized() {
+        require(_to != address(0),"NFT4Play: _to address not a valid");
+        require(counter <= totalSupply,"NFT4Play: maximum tokens minted");
+        counter += 1;
+        tokenFaction[counter] = faction;
+        _holderTokens[_to].add(counter);
+        _safeMint(_to,counter);
+    }
+
+    function batchMint(address _to,uint256 _numberOfToken) external onlyAuthorized() {
+        for (uint256 i = 0; i < _numberOfToken; i++) {
+            mint(_to);
+        }
+    }
+
+    function burn(uint _tokenId) public {
+        require(_isApprovedOrOwner(_msgSender(), _tokenId), "NFT4Play: burn caller is not owner nor approved");
+        _holderTokens[msg.sender].remove(_tokenId);
+        delete tokenFaction[_tokenId];
+        _burn(_tokenId);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI_;
+    }
+
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "NFT4Play: URI query for nonexistent token");
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(),".json")) : "";
+    }
+
+    function transferFrom(address from,address to,uint256 tokenId) public override {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "NFT4Play: transfer caller is not owner nor approved");
+        _holderTokens[from].remove(tokenId);
+        _holderTokens[to].add(tokenId);
+        _transfer(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from,address to,uint256 tokenId) public override {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    function safeTransferFrom(address from,address to,uint256 tokenId,bytes memory _data) public override {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "NFT4Play: transfer caller is not owner nor approved");
+        _holderTokens[from].remove(tokenId);
+        _holderTokens[to].add(tokenId);
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+    function getTokens(address _address) public view returns(uint256[] memory){
+        return _holderTokens[_address].values();
+    }
+
+}
---
+++ contracts/CowSale.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+pragma experimental ABIEncoderV2 ;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+
+interface x22IERC721 {
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+    function tokenOfOwnerByIndex(address _address , uint256 _index) external returns(uint256);
+    function balanceOf(address _address) external returns(uint256);
+    function ownerOf(uint256 _tokenId) external returns(address);
+    function mint(address) external;
+    function batchMint(address,uint256) external; 
+    function faction() external returns(string memory);
+}
+
+interface SwapPair {
+    function getReserves() external view
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+}
+
+contract Purchase is Ownable,Pausable,ReentrancyGuard {
+    
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    
+    SwapPair public x22_eth_Pair;
+    
+    x22IERC721 public nft;
+    IERC20 public x22;
+    
+    address payable public treasurerAddress;
+    
+    uint256 public constant DENOMINATOR = 10000;
+    
+    uint256 public maxTokenForOneSaleTransaction;
+    uint256 public discount;
+    uint256 public priceInETH;
+    uint256 public priceInX22;
+
+    bool public isDiscount;
+
+    event BuyNFTforETH(address _buyer,uint256 _amount,uint256 _numberOfToken);
+    event BuyNFTforX22(address _buyer,uint256 _amount,uint256 _numberOfToken);
+    event UpdateWhitelist(bool _isWhitelist);
+    event UpdateIsDiscount(bool _isDiscount);
+    event SetPrices(uint256 _priceInETH);
+    event SetPricesX22(uint256 _priceInX22);
+    event SetDiscount(uint256 _discount);
+    event SetTokensLimit(uint256 _normalSale);
+    
+    modifier validTokensAmount(uint256 _numberOfToken){
+        require(_numberOfToken <= maxTokenForOneSaleTransaction,"Purchase: enter valid token amount for normal sale");
+        _;
+    }
+
+    constructor(address _nft,address _x22,address _pair) public {
+        require(_nft != address(0));
+        require(_x22 != address(0));
+        nft = x22IERC721(_nft);
+        x22 = IERC20(_x22);
+        x22_eth_Pair = SwapPair(_pair);
+        priceInETH = 60000000000000000;
+        priceInX22 = 100000000000000000000;
+        discount = 5000;
+        maxTokenForOneSaleTransaction = 20;
+        _pause();
+    }
+    
+    function setTokensLimit(uint256 _normalSale) public onlyOwner(){
+        maxTokenForOneSaleTransaction = _normalSale;
+        emit SetTokensLimit(maxTokenForOneSaleTransaction);
+    }
+
+    function updateIsDiscount(bool _isDiscount) public onlyOwner(){
+        isDiscount = _isDiscount;
+        emit UpdateIsDiscount(isDiscount);
+    }
+    
+    function setPrices(uint256 _priceInETH) public onlyOwner() {
+        require(_priceInETH > 0 ,"Purchase: Price must be greater than zero");
+        priceInETH = _priceInETH;
+        emit SetPrices(_priceInETH);
+    }
+
+    function setPricesX22(uint256 _priceInX22) public onlyOwner() {
+        require(_priceInX22 > 0 ,"Purchase: Price must be greater than zero");
+        priceInX22 = _priceInX22;
+        emit SetPricesX22(_priceInX22);
+    }
+    
+    function setDiscount(uint _discount) public onlyOwner(){
+        require(_discount > 0 && _discount <= DENOMINATOR,"Purchase: Discount in not valid ");
+        discount = _discount;
+        emit SetDiscount(discount);
+    }
+    
+    function setTreasurerAddress(address payable _treasurerAddress) public onlyOwner(){
+        require(_treasurerAddress != address(0),"Purchase: _treasurerAddress not be zero address");
+        treasurerAddress = _treasurerAddress ;
+    }
+
+    function batchNFTCreate(address[] memory _addresses) public onlyOwner(){
+        require(_addresses.length != 0,"Purchase: use Valid addresses array ");
+        for (uint256 i = 0; i < _addresses.length; i++) {
+            nft.mint(_addresses[i]);
+        }
+    }
+
+    function pause() public onlyOwner(){
+        _pause();
+    }
+    
+    function unpause() public onlyOwner(){
+        _unpause();
+    }
+    
+    
+    function buyWithETH(uint256 _numberOfToken) public payable nonReentrant() validTokensAmount(_numberOfToken) whenNotPaused(){
+        require (_numberOfToken > 0 ,"Purchase: _numberOfToken must be greater than zero");
+        require(msg.value >= priceInETH.mul(_numberOfToken),"Purchase: amount of ETH must be equal to token price" ); 
+        uint256 extraAmount = msg.value.sub(priceInETH.mul(_numberOfToken));
+        if(_safeTransferETH(treasurerAddress,priceInETH.mul(_numberOfToken))){
+            nft.batchMint(msg.sender,_numberOfToken);    
+        }
+        _safeTransferETH(msg.sender,extraAmount);
+        emit BuyNFTforETH(msg.sender,priceInETH.mul(_numberOfToken),_numberOfToken);
+    }
+    
+    function buyWithX22(uint256 amount,uint256 _numberOfToken) public nonReentrant() validTokensAmount(_numberOfToken) whenNotPaused(){
+        require (_numberOfToken > 0 ,"Purchase: _numberOfToken must be greater than zero");
+        uint256 _amount;
+        if(isDiscount){
+            _amount = amountToPay().mul(_numberOfToken);
+            require(amount >= _amount.mul(9500).div(DENOMINATOR), "Purchase: amount of x22 must be equal to token price");
+        }
+        else{
+            _amount = priceInX22.mul(_numberOfToken);
+            require(amount >= _amount, "Purchase: amount of x22 must be equal to token price");
+        }
+        x22.transferFrom(msg.sender, treasurerAddress, _amount);
+        nft.batchMint(msg.sender,_numberOfToken);
+        emit BuyNFTforX22(msg.sender,_amount,_numberOfToken);
+    }
+    
+    function amountToPay() public view returns(uint256) {
+        uint256 temp = priceInETH.mul(DENOMINATOR.sub(discount)).div(DENOMINATOR);
+        return getPriceInx22(temp);
+    }
+    
+    function getPriceInx22(uint256 _amount) public view returns(uint256){
+        (uint256 reserve0,uint256 reserve1,) = x22_eth_Pair.getReserves();
+        uint256 temp = reserve0.mul(10**18).div(reserve1);
+        return temp.mul(_amount).div(10**18);
+    }
+    
+    function _safeTransferETH(address to, uint256 value) internal returns (bool) {
+        (bool success, ) = to.call{value: value}(new bytes(0));
+        return success;
+    }
+}
